907119e01f8728bcf0392b593fcc88d420a98c8e
Chingiz-888 <servicetrade0@gmail.com>
2016-12-26 16:10:23 +0300

    initial commit

---
+++ upload.php
@@ -0,0 +1,64 @@
+<?php
+
+/**
+ *   Поменняйте PROJECT на нужную вам директорию на вашем хосте  (или уберите
+ *   эту константу, если скрипт будет лежать в коре)
+ *   UPLOAD_DIR - на ту директорию куда будут выигружаться картинки
+ *
+ *   Внимание! Не забудьте создать директорию, указанную в UPLOAD_DIR на сервере 
+ *
+ */
+
+
+define('UPLOAD_DIR', 'uploads/');
+define('PROJECT', '/test_images_upload/');
+
+//на случай тестирования на MAMP'е/XAMP'e и им подобных локальных веб-серверах
+//мы еще смотрим порт (эти локальные сервера запускаются на 3000 или 8888 портах, а не на 80)
+$port = $_SERVER['SERVER_PORT'];
+$port_ext = ($port != NULL && $port != '80') ?  ':'.$port : '';
+
+//получаем абсолютный адрес с хостом, портом, для точных ссылок на загруженные рисунки
+$address =   "http://".$_SERVER["SERVER_NAME"].$port_ext.PROJECT; 
+
+header('Content-Type: application/json');
+header("Access-Control-Allow-Origin: *");
+
+
+$uploaded      = array();
+$success       = false;
+
+
+
+//как минимум хотя бы один файл должен быть отправлен
+if(!empty($_FILES['file']['name'][0]))  {
+	//перебираем
+	foreach ($_FILES['file']['name'] as $position => $name) {
+		if(move_uploaded_file($_FILES['file']['tmp_name'][$position], UPLOAD_DIR.$_FILES['file']['name'][$position])){
+			$uploaded[] = array(
+				"name" => $name,
+				"url" =>  $address.UPLOAD_DIR.$name
+			);
+			$success = true;
+		}	
+	}
+} 
+
+//готтовим json ответа, используем переменную-флаг $success
+if($success) {
+	$json_response = array(
+			"result" => "1",
+			"message" => "All images was successfully uploaded on server",
+			"uploaded_images" => $uploaded
+		);
+} else {
+	$json_response = array(
+			"result" => "0",
+			"message" => "There were some errors while images uploading on server"
+		);
+}
+
+
+echo json_encode($json_response);
+
+?>
