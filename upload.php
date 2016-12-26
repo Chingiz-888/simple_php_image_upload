63757769949c46ea09f4abce79e5c676a56382e7
Chingiz-888 <servicetrade0@gmail.com>
2016-12-26 20:13:01 +0300

    php image proccessing included

--- upload.php
+++ upload.php
@@ -12,6 +12,7 @@
 
 define('UPLOAD_DIR', 'uploads/');
 define('PROJECT', '/test_images_upload/');
+define('RESTRICT_IMAGE_SIZE', 800);
 
 //на случай тестирования на MAMP'е/XAMP'e и им подобных локальных веб-серверах
 //мы еще смотрим порт (эти локальные сервера запускаются на 3000 или 8888 портах, а не на 80)
@@ -34,14 +35,33 @@ $success       = false;
 if(!empty($_FILES['file']['name'][0]))  {
 	//перебираем
 	foreach ($_FILES['file']['name'] as $position => $name) {
-		if(move_uploaded_file($_FILES['file']['tmp_name'][$position], UPLOAD_DIR.$_FILES['file']['name'][$position])){
-			$uploaded[] = array(
-				"name" => $name,
-				"url" =>  $address.UPLOAD_DIR.$name
-			);
-			$success = true;
-		}	
-	}
+
+		//меняем размеры
+		$fn = $_FILES['file']['tmp_name'][$position];
+		$size = getimagesize($fn);
+		$ratio = $size[0]/$size[1]; // width/height
+		if( $ratio > 1) {
+		    $width = RESTRICT_IMAGE_SIZE;
+		    $height = RESTRICT_IMAGE_SIZE/$ratio;
+		} else {
+		    $width = RESTRICT_IMAGE_SIZE*$ratio;
+		    $height = RESTRICT_IMAGE_SIZE;
+		}
+
+		$src = imagecreatefromstring(file_get_contents($fn));
+		$dst = imagecreatetruecolor($width, $height);
+		imagecopyresampled($dst, $src, 0, 0, 0, 0, $width, $height, $size[0], $size[1]);
+		imagedestroy($src);
+		$target_filename_here = UPLOAD_DIR.$_FILES['file']['name'][$position];
+		imagepng($dst, $target_filename_here); // adjust format as needed
+		imagedestroy($dst);
+
+		$uploaded[] = array(
+			"name" => $name,
+			"url" =>  $address.UPLOAD_DIR.$name
+		);
+		$success = true;
+		}//end of foreach cycle	
 } 
 
 //готтовим json ответа, используем переменную-флаг $success
